1754935db18f2b0ed8100e0ddf6ae8c57a42fc76
Your Name <your-email@example.com>
2026-09-24 09:44:27 +0530

    updated

--- devops-project-1/backup_delete_route.py
+++ devops-project-1/backup_delete_route.py
@@ -1,7 +1,7 @@
 import subprocess
 import json
 
-ROUTE_TABLE_ID = "rtb-0123456789abcdef"
+ROUTE_TABLE_ID = "rtb-0ad7b9c996c2e8465"
 DESTINATION = "10.20.0.0/16"
 
 
@@ -13,7 +13,6 @@ def run_aws_command(command):
     )
 
     if result.returncode != 0:
-        print("AWS CLI command failed:")
         print(result.stderr)
         raise SystemExit(1)
 
@@ -34,4 +33,17 @@ output = run_aws_command(command)
 
 data = json.loads(output)
 
-print(json.dumps(data, indent=4))
+routes = data["RouteTables"][0]["Routes"]
+
+for route in routes:
+
+    destination = (
+        route.get("DestinationCidrBlock")
+        or route.get("DestinationIpv6CidrBlock")
+        or route.get("DestinationPrefixListId")
+    )
+
+    if destination == DESTINATION:
+
+        print("Route found:")
+        print(json.dumps(route, indent=4))
